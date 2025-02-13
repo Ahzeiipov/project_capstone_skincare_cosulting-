492f8fd7d6ae8c16bb102b5de49221704e8edb4f
sreypov <sreypov.poch@student.cadt.edu.kh>
2025-02-13 21:50:07 +0700

    updated

---
+++ project_capstone_skincare_cosulting--main/project_capstone_skincare_cosulting--main/front-end/navigation/navigation.js
@@ -0,0 +1,15 @@
+// // navigation-bar.js (inside the 'navigation-bar' folder)
+// document.addEventListener("DOMContentLoaded", () => {
+//     const navLinks = document.querySelectorAll('.navbar .menu li a');
+
+//     // Add click event listener to each link
+//     navLinks.forEach(link => {
+//         link.addEventListener('click', function() {
+//             // Remove 'active' class from all links
+//             navLinks.forEach(link => link.classList.remove('active'));
+
+//             // Add 'active' class to the clicked link
+//             this.classList.add('active');
+//         });
+//     });
+// });
---
+++ project_capstone_skincare_cosulting--main/project_capstone_skincare_cosulting--main/front-end/page/slider.js
@@ -0,0 +1,55 @@
+const slides = document.querySelectorAll('.slides');
+const dots = document.querySelectorAll('.dot')
+
+
+let index = 0;
+
+// Adding opacity to first dot on first time
+
+dots[0].style.opacity = '1'
+
+// positioning the slides
+
+slides.forEach((slide, index) => {
+    slide.style.left = `${index * 100}%`
+});
+
+
+// move slide function
+
+const moveSlide = () => {
+    slides.forEach((slide) => {
+        slide.style.transform = `translateX(-${index * 100}%)`;
+    });
+}
+
+// remove dots opacity 1 from all dots
+
+const removeDotsOpacity = () => {
+    dots.forEach((dot) => {
+        dot.style.opacity = '.2';
+    });
+}
+
+dots.forEach((dot, i) => {
+    dot.addEventListener("click", (e) => {
+        index = i;
+        removeDotsOpacity();
+        e.target.style.opacity = '1'
+        moveSlide();
+    })
+});
+
+// auto play slide
+
+const autoPlaySlide = () => {
+    removeDotsOpacity();
+    if (index === slides.length - 1) index = -1;
+    index++;
+    dots[index].style.opacity = '1'
+    moveSlide();
+}
+
+window.onload = () => {
+    setInterval(autoPlaySlide, 6000);
+}
