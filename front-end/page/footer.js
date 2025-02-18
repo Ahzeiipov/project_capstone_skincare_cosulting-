67bed4896e8bbe7c0cdb05329547c2e432de396e
lenovo <ssopheakline@gmail.com>
2025-02-18 09:16:54 +0700

    update

---
+++ front-end/page/footer.js
@@ -0,0 +1,10 @@
+// homepage.js (located inside the 'pages' folder)
+document.addEventListener("DOMContentLoaded", function() {
+    // Fetch the footer HTML and inject it into the footer div
+    fetch('../../front-end/footer/footer.html') // Adjust the path to navigate to the 'footer' folder
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('footer').innerHTML = data;
+      })
+      .catch(error => console.error('Error loading footer:', error));
+});
